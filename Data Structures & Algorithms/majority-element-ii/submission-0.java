026f2b0d89b3eae1a02ac585cbdd2a42ecfcc730
Manikandan R <51197059+manikandanravi94@users.noreply.github.com>
2026-04-01 07:52:48 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+
+        int cand1=0,cand2=1,count1=0,count2=0;
+
+        for(int n: nums){
+
+            if(n==cand1){
+                count1++;
+            }else if(n==cand2){
+                count2++;
+            }else if(count1==0){
+                cand1=n;
+                count1=1;
+            }else if(count2==0){
+                cand2=n;
+                count2=1;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1=0;
+        count2=0;
+
+        for(int n:nums){
+            if(n==cand1)
+            count1++;
+            if(n==cand2)
+            count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n=nums.length;
+        if(count1>n/3) result.add(cand1);
+        if(count2>n/3) result.add(cand2);
+
+        return result;
+
+    }
+}
