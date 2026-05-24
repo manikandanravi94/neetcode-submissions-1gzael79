0815192b1ce827f9850117f34b9be578c237d20c
Manikandan R <51197059+manikandanravi94@users.noreply.github.com>
2026-05-24 11:23:09 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Character> st = new LinkedList<>();
+        
+        for(char ch:s.toCharArray()){
+            if(ch==']'){
+                StringBuilder sb = new StringBuilder();
+                while(!st.isEmpty() && st.peekLast()!='['){
+                    sb.append(st.pollLast());
+                }
+                st.pollLast();
+                StringBuilder integerS = new StringBuilder();
+                while(!st.isEmpty() && Character.isDigit(st.peekLast())){
+                   integerS.append(st.pollLast());
+                }
+                Integer count = Integer.parseInt(integerS.reverse().toString());
+                String op = sb.reverse().toString();
+                for(int i=0;i<count;i++){
+                    for(char c:op.toCharArray()){
+                        st.addLast(c);
+                    }
+                }
+            }else{
+                st.addLast(ch);
+            }
+
+        }
+        StringBuilder result = new StringBuilder();
+        while(!st.isEmpty()){
+            result.append(st.pollFirst());
+        }
+        return result.toString();
+    }
+}
